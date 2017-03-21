1805d70e322ac2dd4fad1b5ae5452e8ccaff1fc7
pututampan27 <sanjaya272001@gmail.com>
2017-03-22 00:48:37 +0700

    Add files via upload

---
+++ jam(mahirberkomputer.com).js
@@ -0,0 +1,23 @@
+/* js mahirberkomputer.com */
+<div id="clock"></div>
+<script type="text/javascript">
+<!--
+function startTime() {
+    var today=new Date(),
+        curr_hour=today.getHours(),
+        curr_min=today.getMinutes(),
+        curr_sec=today.getSeconds();
+ curr_hour=checkTime(curr_hour);
+    curr_min=checkTime(curr_min);
+    curr_sec=checkTime(curr_sec);
+    document.getElementById('clock').innerHTML=curr_hour+":"+curr_min+":"+curr_sec;
+}
+function checkTime(i) {
+    if (i<10) {
+        i="0" + i;
+    }
+    return i;
+}
+setInterval(startTime, 500);
+//-->
+</script>
