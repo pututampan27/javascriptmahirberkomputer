0dc50e785c59f6fd217c0db6eaa71b3abc9c7d4e
pututampan27 <sanjaya272001@gmail.com>
2017-03-22 15:12:21 +0700

    Add files via upload

---
+++ salam(mahirberkomputer.com).js
@@ -0,0 +1,14 @@
+/* js mahirberkomputer.com */
+now=new Date();
+jam=now.getUTCHours();
+if (jam < 3){document.write("Selamat Pagi...!") }
+else {if (jam < 9){document.write("Selamat Siang...!") }
+else {if (jam < 11){document.write("Selamat Sore...!") }
+else {if (jam < 16){document.write("Selamat Malam...!") }
+else {if (jam < 22){document.write("Begadang Boss...!") }
+else {document.write("Selamat Pagi...!") }
+}
+}
+}
+}
+//-->/*HDK*/
